cb3dffe20b41801f232cd0ce80783f74b00670c7
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-18 14:27:24 -0400

    add file ArticleDeRecherche.java and code

---
+++ src/ArticleDeRecherche.java
@@ -0,0 +1,10 @@
+public class ArticleDeRecherche extends Document {
+    public ArticleDeRecherche(int id, String titre, String auteur) {
+        super(id, titre, auteur);
+    }
+
+    @Override
+    public String toString() {
+        return "[Article de recherche]--> " + super.toString();
+    }
+}
