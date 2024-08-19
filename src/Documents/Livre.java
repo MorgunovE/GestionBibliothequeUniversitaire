5581b58e33a927d496284104265e05e3aef91d16
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-19 09:35:50 -0400

    add doc in Livre.java

--- src/Documents/Livre.java
+++ src/Documents/Livre.java
@@ -1,10 +1,27 @@
 package Documents;
 
+/**
+ * Classe représentant un livre.
+ */
 public class Livre extends Document {
+
+    /**
+     * Constructeur de la classe Livre.
+     *
+     * @param id     L'identifiant du livre.
+     * @param titre  Le titre du livre.
+     * @param auteur L'auteur du livre.
+     * @throws IllegalArgumentException si l'ID est inférieur à 1, ou si le titre ou l'auteur est vide.
+     */
     public Livre(int id, String titre, String auteur) {
         super(id, titre, auteur);
     }
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères du livre.
+     *
+     * @return Une chaîne de caractères représentant le livre.
+     */
     @Override
     public String toString() {
         return "[Livre]--> " + super.toString();
