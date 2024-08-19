8533f0dc90d08670dd947d2471497d0c28c5051f
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-19 09:36:44 -0400

    add doc in Revue.java

--- src/Documents/Revue.java
+++ src/Documents/Revue.java
@@ -1,10 +1,27 @@
 package Documents;
 
+/**
+ * Classe représentant une revue.
+ */
 public class Revue extends Document {
+
+    /**
+     * Constructeur de la classe Revue.
+     *
+     * @param id     L'identifiant de la revue.
+     * @param titre  Le titre de la revue.
+     * @param auteur L'auteur de la revue.
+     * @throws IllegalArgumentException si l'ID est inférieur à 1, ou si le titre ou l'auteur est vide.
+     */
     public Revue(int id, String titre, String auteur) {
         super(id, titre, auteur);
     }
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de la revue.
+     *
+     * @return Une chaîne de caractères représentant la revue.
+     */
     @Override
     public String toString() {
         return "[Revue]--> " + super.toString();
